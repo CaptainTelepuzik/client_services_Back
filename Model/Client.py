11b1707219e63989ba3c2eeed274f6c865b99060
CaptainTelepuzik <106181698+CaptainTelepuzik@users.noreply.github.com>
2023-02-27 23:02:48 +0500

    27/02 23:02

--- Model/Client.py
+++ Model/Client.py
@@ -9,11 +9,11 @@ class Client(BaseModel):
     __tablename__ = 'clients'
 
     user_id = Column(Integer, ForeignKey('users.id'))
-    client_surname = Column(Text, nullable=False)
+    client_surname = Column(Text)
     client_name = Column(Text, nullable=False)
-    client_firstname = Column(Text, nullable=False)
+    client_firstname = Column(Text)
     client_telephone = Column(Text, nullable=False, unique=True)
-    client_last_telephone = Column(Text, nullable=False, unique=True)
+    client_last_telephone = Column(Text, unique=True)
 
     user = relationship('User', foreign_keys=[user_id])
 
