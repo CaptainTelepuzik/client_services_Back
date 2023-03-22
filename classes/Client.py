1361869c9e696a58e3a18361e339035485e1b716
CaptainTelepuzik <106181698+CaptainTelepuzik@users.noreply.github.com>
2023-03-23 00:17:48 +0500

    23 фильтр сделал

--- classes/Client.py
+++ classes/Client.py
@@ -7,3 +7,12 @@ class Client(BaseClass):
     def get_model(new_model: bool = False) -> ClientModel:
         return ClientModel() if new_model else ClientModel
 
+    def _prepare_query_filter(self, query, filter_params):
+
+        if filter_params.get('searchTelephone'):
+            value = filter_params.get('searchTelephone')
+            query = query.where(self.get_model().client_telephone == value)
+
+        return query
+
+
