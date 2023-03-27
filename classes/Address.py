dcbbc8cceca5d68ce477618d92ee75cba331bd67
CaptainTelepuzik <106181698+CaptainTelepuzik@users.noreply.github.com>
2023-03-27 21:13:45 +0500

    27

--- classes/Address.py
+++ classes/Address.py
@@ -7,6 +7,15 @@ class Address(BaseClass):
     def get_model(new_model: bool = False) -> AddressModel:
         return AddressModel() if new_model else AddressModel
 
+    def _prepare_query_filter(self, query, filter_params):
+
+        if filter_params.get('searchAddress'):
+            value = filter_params.get('searchAddress')
+            query = query.where(self.get_model().street_name == value)
+            return query
+
+        else:
+            return
 
 
 
