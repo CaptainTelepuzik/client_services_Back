c2979a524afc1e7f1c9ba409876fdeda5a4a9f6e
CaptainTelepuzik <106181698+CaptainTelepuzik@users.noreply.github.com>
2023-07-21 20:42:07 +0500

    add Filtr Data

--- classes/Measurement.py
+++ classes/Measurement.py
@@ -8,7 +8,12 @@ class Measurement(BaseClass):
     def get_model(new_model: bool = False) -> MeasurementModel:
         return MeasurementModel() if new_model else MeasurementModel
 
+    def _prepare_query_filter(self, query, filter_params):
 
+        if filter_params.get('selectedDay'):
+            value = filter_params.get('selectedDay')
+            query = query.where(self.get_model().date_measure == value)
+            return query
 
 
 
